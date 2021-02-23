53fbcb10a1b7f837dc6ac49dc75a606498ff770b
parimalmahindrakar <parimalm4653@gmail.com>
2021-02-23 20:21:45 +0530

    learnr some new

--- js/app.js
+++ js/app.js
@@ -1,15 +1,28 @@
-var circleObject = {
-    height: 20,
-    width: 20,
-}
+var col = 0;
 function setup() {
-    createCanvas(1920, 1000)
-    background(196)
+    createCanvas(600,600)
 }
 
 function draw() {
-    ellipse(960, 540, circleObject.height, circleObject.width)
-    circleObject.height++
-     circleObject.width++
+    col = map(mouseX, 0, 600, 0, 255);
+    background(col)
+
+    fill(250, 118, 222)
+    ellipse(mouseX, 200, 64, 64);
+}
+
+/**
+ 
+    What does the map function does ?
+    => It takes the 5 arguments.
+     ex, map(a,b,c,d,e);
+     here ,
+            a       =>  The value with respect to !
+            b ,c    =>  The existed range
+            e,f     =>  The range in which we want to output !
     
-}   
+    see this lecture , 
+    https: //youtu.be/nicMAoW6u1g?list=PLglp04UYZK_PrN6xWo_nJ-8kzyXDyFUwi
+ 
+  
+ */
