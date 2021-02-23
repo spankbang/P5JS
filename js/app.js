8d6cd66979e6d2f67f21cd38bac7537d6a7ef621
parimalmahindrakar <parimalm4653@gmail.com>
2021-02-23 15:15:09 +0530

    added very basic code !

--- js/app.js
+++ js/app.js
@@ -1,10 +1,14 @@
 function setup() {
     createCanvas(600, 600)
+    // this is the 600x600 pixels wide square !
     
 }
 
 function draw() {
 
-    background(51)
+    // background(51)
+    // 51 represendts the colors of background 
 
+    background(74, 96, 221)
+    // this is RGB color code !
 }
